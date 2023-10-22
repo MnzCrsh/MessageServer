e6e07aa5564c9caf8ec1f707ee79736475249fc2
MnzCrsh <96451040+MnzCrsh@users.noreply.github.com>
2023-10-22 22:22:41 +0300

    Added Di Extension method

---
+++ src/MessageServer/MessageServer/DiServicesExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace MessageServer;
+
+public static class DiServicesExtension
+{
+    public static void RunServices(this IServiceCollection services)
+    {
+        services.AddControllers().AddNewtonsoftJson();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddHsts(options =>
+        {
+            const int maxTime = 1;
+            options.MaxAge = TimeSpan.FromHours(maxTime);
+        });
+    }
+}

120fa8da1e66fca35fd1b42c9961b903daa3b66a
MnzCrsh <96451040+MnzCrsh@users.noreply.github.com>
2024-02-05 00:36:41 +0300

    Added initial migration

---
+++ src/MessageServer/ProducerMessageServer/Migrations/20240204213357_InitialMigration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessageServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PetOwners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    IsMarkedToDelete = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetOwners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PetDto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OwnedById = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    PetAge = table.Column<int>(type: "integer", nullable: false),
+                    IsMarkedToDelete = table.Column<bool>(type: "boolean", nullable: false),
+                    PetOwnerDtoId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetDto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PetDto_PetOwners_PetOwnerDtoId",
+                        column: x => x.PetOwnerDtoId,
+                        principalTable: "PetOwners",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetDto_PetOwnerDtoId",
+                table: "PetDto",
+                column: "PetOwnerDtoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PetDto");
+
+            migrationBuilder.DropTable(
+                name: "PetOwners");
+        }
+    }
+}

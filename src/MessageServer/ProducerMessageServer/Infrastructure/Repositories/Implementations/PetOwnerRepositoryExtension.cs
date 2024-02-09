5cd1568c69bf7eb8090b7d19c8824b8366de6812
MnzCrsh <96451040+MnzCrsh@users.noreply.github.com>
2024-02-09 23:31:12 +0300

    Changed PetOwnerRepositoryExtension.cs

--- src/MessageServer/ProducerMessageServer/Infrastructure/Repositories/Implementations/PetOwnerRepositoryExtension.cs
+++ src/MessageServer/ProducerMessageServer/Infrastructure/Repositories/Implementations/PetOwnerRepositoryExtension.cs
@@ -1,22 +1,29 @@
-﻿using MessageServer.Infrastructure.Repositories.Interfaces;
+﻿using MessageServer.Domain;
+using MessageServer.Infrastructure.Repositories.Interfaces;
 
 namespace MessageServer.Infrastructure.Repositories.Implementations;
 
 public static class PetOwnerRepositoryExtension
 {
-    public static async Task AddPetToOwnerAsync(this IPetOwnerRepository ownerRepo,
+    public static async Task AddPetAsync(this IPetOwnerRepository ownerRepo,
                                                      IPetRepository petRepo,int ownerId, int petId)
     {
-        var owner = await ownerRepo.GetAsync(ownerId);
-        var pet = await petRepo.GetAsync(petId);
+        var (owner,pet) = await GetOwnerAndPetAsync(ownerRepo, petRepo, ownerId, petId);
         owner.OwnedPets?.Add(pet);
     }
 
-    public static async Task RemovePetFromOwnerAsync(this IPetOwnerRepository ownerRepo,
-        IPetRepository petRepo,int ownerId, int petId)
+    public static async Task RemovePetAsync(this IPetOwnerRepository ownerRepo,
+                                                          IPetRepository petRepo,int ownerId, int petId)
+    {
+        var (owner,pet) = await GetOwnerAndPetAsync(ownerRepo, petRepo, ownerId, petId);
+        owner.OwnedPets?.Remove(pet);
+    }
+    
+    private static async Task<(PetOwnerDto, PetDto)> GetOwnerAndPetAsync(
+        this IPetOwnerRepository ownerRepo, IPetRepository petRepo, int ownerId, int petId)
     {
         var owner = await ownerRepo.GetAsync(ownerId);
         var pet = await petRepo.GetAsync(petId);
-        owner.OwnedPets?.Remove(pet);
+        return (owner, pet);
     }
 }
